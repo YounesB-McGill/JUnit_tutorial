50bdbfc0d5420cd250d334f0f777599ca13840c6
Younes Boubekeur <younes.boubekeur@mail.mcgill.ca>
2020-10-14 01:09:04 -0400

    Remove TODOs

--- src/ca/mcgill/ecse211/test/TestPoint.java
+++ src/ca/mcgill/ecse211/test/TestPoint.java
@@ -10,7 +10,7 @@ public class TestPoint {
 
   /** Tests Point.makePointsFromString(). */
   @Test void testMakePointsFromString() {
-    // TODO Test usual case
+    // Test usual case
     var expected = List.of(p(1, 2));
     var actual = Point.makePointsFromString("(1, 2)");
     assertEquals(expected, actual);
@@ -19,7 +19,7 @@ public class TestPoint {
     actual = Point.makePointsFromString("(1, 2), (2.5, 7.5), (-7, -6.0)");
     assertEquals(expected, actual);
 
-    // TODO Test edge cases
+    // Test edge cases
     expected = List.of();
     actual = Point.makePointsFromString("");
     assertEquals(expected, actual);
